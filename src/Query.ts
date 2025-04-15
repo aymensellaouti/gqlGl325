616aeb9089e20675d72e75ea2c18764cdfd0955f
aymensellaouti <aymen.sellaouti@gmail.com>
2025-04-15 12:45:02 +0100

    add GraphQl Error

--- src/Query.ts
+++ src/Query.ts
@@ -1,8 +1,22 @@
+import { GraphQLError } from "graphql";
 import { DB } from "./db/db";
 
 export const Query = {
   hello: () => "Hello GL3  !!!",
   users: () => DB.users,
+  user: (parent, { id }, context, info) => {
+    const user = DB.users.find(user => user.id == id);
+    if (!user) {
+      throw new GraphQLError(`Element with id '${id}' not found.`, {
+        extensions: {
+          http: {
+            status: 404
+          }
+        }
+      });
+    }
+    return user;
+  } ,
   workers: (parent, { isWorking = false }, context, info) =>
     DB.users.filter((user) => user.isWorking == isWorking),
   roles: () => DB.roles       
